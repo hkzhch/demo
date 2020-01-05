d376d0e68295553c1e665558b9199a8b602691c4
ZhangChen <clegec1204@gmail.com>
2020-01-05 17:10:11 +0900

    add interceptor

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,15 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import com.example.demo.dto.PaginationDTO;
-import com.example.demo.mapper.UserMapper;
-import com.example.demo.model.User;
 import com.example.demo.service.QusetionService;
 
 @Controller
 public class IndexController {
 
-	@Autowired
-	private UserMapper userMapper;
 	@Autowired
 	private QusetionService questionService;
 
@@ -27,7 +22,7 @@ public class IndexController {
 			Model model,
 			@RequestParam(name = "page" ,defaultValue = "1")Integer page,
 			@RequestParam(name = "size" ,defaultValue = "5")Integer size) {
-		Cookie[] cookies = request.getCookies();
+		/*Cookie[] cookies = request.getCookies();
 		if(cookies !=null) {
 			for (Cookie cookie : cookies) {
 				if (cookie.getName().equalsIgnoreCase("token")) {
@@ -39,7 +34,7 @@ public class IndexController {
 					break;
 				}
 			}
-		}
+		}*/
 		PaginationDTO pagination=questionService.list(page,size);
 		model.addAttribute("pagination", pagination);
 		return "index";
--- src/main/java/com/example/demo/controller/ProfileController.java
+++ src/main/java/com/example/demo/controller/ProfileController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,21 +29,8 @@ public class ProfileController {
 			HttpServletRequest request,
 			@RequestParam(name = "page", defaultValue = "1") Integer page,
 			@RequestParam(name = "size", defaultValue = "5") Integer size) {
-		User user = null;
-		Cookie[] cookies = request.getCookies();
-		if (cookies != null && cookies.length != 0) {
-			for (Cookie cookie : cookies) {
-				if (cookie.getName().equalsIgnoreCase("token")) {
-					String token = cookie.getValue();
-					user = userMapper.findByToken(token);
-					if (user != null) {
-						request.getSession().setAttribute("user", user);
-					}
-					break;
-				}
-			}
-		}
 
+		User user =(User) request.getSession().getAttribute("user");
 		if (user == null) {
 			return "redirect:/";
 		}
